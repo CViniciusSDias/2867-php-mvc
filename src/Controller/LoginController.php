ae149be37d87204933a419ce0954d4653d8d3c11
Vinicius Dias <carlosv775@gmail.com>
2022-11-05 23:32:07 -0300

    Correção Video 4.1

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -27,14 +27,14 @@ class LoginController implements Controller
         $userData = $statement->fetch(\PDO::FETCH_ASSOC);
         $correctPassword = password_verify($password, $userData['password'] ?? '');
 
-        if (password_needs_rehash($userData['password'], PASSWORD_ARGON2ID)) {
-            $statement = $this->pdo->prepare('UPDATE users SET password = ? WHERE id = ?');
-            $statement->bindValue(1, password_hash($password, PASSWORD_ARGON2ID));
-            $statement->bindValue(2, $userData['id']);
-            $statement->execute();
-        }
-
         if ($correctPassword) {
+            if (password_needs_rehash($userData['password'], PASSWORD_ARGON2ID)) {
+                $statement = $this->pdo->prepare('UPDATE users SET password = ? WHERE id = ?');
+                $statement->bindValue(1, password_hash($password, PASSWORD_ARGON2ID));
+                $statement->bindValue(2, $userData['id']);
+                $statement->execute();
+            }
+
             $_SESSION['logado'] = true;
             header('Location: /');
         } else {
