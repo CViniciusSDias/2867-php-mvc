640303abbc9bc8f1aca1aa90711f6326412adc51
Vinicius Dias <carlosv775@gmail.com>
2022-11-05 21:39:33 -0300

    Video 3.4

--- src/Repository/VideoRepository.php
+++ src/Repository/VideoRepository.php
@@ -40,12 +40,24 @@ class VideoRepository
 
     public function update(Video $video): bool
     {
-        $sql = 'UPDATE videos SET url = :url, title = :title WHERE id = :id;';
+        $updateImageSql = '';
+        if ($video->getFilePath() !== null) {
+            $updateImageSql = ', image_path = :image_path';
+        }
+        $sql = "UPDATE videos SET
+                  url = :url,
+                  title = :title
+                $updateImageSql
+              WHERE id = :id;";
         $statement = $this->pdo->prepare($sql);
 
         $statement->bindValue(':url', $video->url);
         $statement->bindValue(':title', $video->title);
         $statement->bindValue(':id', $video->id, PDO::PARAM_INT);
+
+        if ($video->getFilePath() !== null) {
+            $statement->bindValue(':image_path', $video->getFilePath());
+        }
 
         return $statement->execute();
     }
@@ -77,6 +89,10 @@ class VideoRepository
     {
         $video = new Video($videoData['url'], $videoData['title']);
         $video->setId($videoData['id']);
+
+        if ($videoData['image_path'] !== null) {
+            $video->setFilePath($videoData['image_path']);
+        }
 
         return $video;
     }
--- views/video-list.php
+++ views/video-list.php
@@ -6,10 +6,16 @@ require_once __DIR__ . '/inicio-html.php';
 <ul class="videos__container">
     <?php foreach ($videoList as $video): ?>
         <li class="videos__item">
+            <?php if ($video->getFilePath() !== null): ?>
+            <a href="<?= $video->url; ?>">
+                <img src="/img/uploads/<?= $video->getFilePath(); ?>" alt="" style="width: 100%" />
+            </a>
+            <?php else: ?>
             <iframe width="100%" height="72%" src="<?= $video->url; ?>"
                     title="YouTube video player" frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen></iframe>
+            <?php endif; ?>
             <div class="descricao-video">
                 <h3><?= $video->title; ?></h3>
                 <div class="acoes-video">
